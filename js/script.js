7dbc9244a0508e48c2428e392c05fe91fd859c61
NixonXC <invasionyt19@gmail.com>
2022-05-13 09:57:48 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const typed = new Typed(".typing", {
+    strings: [
+    "⚡ Not born to be the Best but want to<br> be better than the Best!",
+    "👑 Outer Shell Black, Inner Shell White!",
+    "🎸 Socitey Treats Life as a Stock,<br> Connect with The Person When Rising,<br> Get Away when its Declining.",
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    loopCount: Infinity,
+    backDelay: 2000,
+    showCursor: true,
+});
+document.getElementById("nyt").onclick = function() {
+    window.open("https://discord.gg/34Un6KrbCj");
+}
